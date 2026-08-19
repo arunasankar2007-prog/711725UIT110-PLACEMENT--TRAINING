72ca300873ef21a584af54fad4e0a3239e84e98a
arunasankar2007-prog <arunasankar2007@gmail.com>
2026-08-19 21:23:22 +0530

    Create RemoveLastDigit.java

---
+++ RemoveLastDigit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RemoveLastDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int result = num / 10;
+
+        System.out.println("After removing last digit = " + result);
+
+        sc.close();
+    }
+}
